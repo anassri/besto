65ebca09ed4c177a9eb4c43d856f1ece666316e7
Ammar-Nassr-ABF1190_ferg <ammar.nassri@ferguson.com>
2025-04-03 13:13:09 -0400

    fixed card size on desktop

--- src/components/dog-card/dog-card.component.tsx
+++ src/components/dog-card/dog-card.component.tsx
@@ -39,7 +39,7 @@ export const DogCard: FunctionComponent<DogCardProps> = ({
     };
     return (
         <Card
-            className={`w-full max-w-full md:max-w-[48%] lg:max-w-[350px] ${
+            className={`w-full max-w-full md:max-w-[48%] lg:min-w-[300px] lg:w-[32%] ${
                 isDogFavorited ? "box-border border-1 border-[#d32f2f]" : ""
             }`}
         >
@@ -50,7 +50,7 @@ export const DogCard: FunctionComponent<DogCardProps> = ({
                 width="350"
                 image={dog.img}
                 alt={`${dog.name} the dog`}
-                className="h-[230px] max-w-fullmd:max-w-[48%] lg:max-w-[350px]"
+                className="h-[230px] max-w-full lg:max-w-[350px]"
             />
             <CardContent>
                 <Typography variant="body2" sx={{ color: "text.secondary" }}>
